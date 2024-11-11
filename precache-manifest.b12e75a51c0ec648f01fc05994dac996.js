c50c45ebebb74256d88b0339976793bb3bf446d0
pagadala002 <pagadala002@gannon.edu>
2024-11-11 08:04:20 -0500

    Updates

---
+++ precache-manifest.b12e75a51c0ec648f01fc05994dac996.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22dfdd12a461c393d35109555cf1e2fa",
+    "url": "/wiseshare-signup-debug/index.html"
+  },
+  {
+    "revision": "02ec8000b1b3aeef8bbc",
+    "url": "/wiseshare-signup-debug/static/css/main.98bf53a7.chunk.css"
+  },
+  {
+    "revision": "1802e54c9c57e7dc1da4",
+    "url": "/wiseshare-signup-debug/static/js/2.a3a4c6ba.chunk.js"
+  },
+  {
+    "revision": "67ae3e0212c3a537d86a1f099983ebeb",
+    "url": "/wiseshare-signup-debug/static/js/2.a3a4c6ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "02ec8000b1b3aeef8bbc",
+    "url": "/wiseshare-signup-debug/static/js/main.8bcda32c.chunk.js"
+  },
+  {
+    "revision": "1fbd9bff2d47584dcdd7",
+    "url": "/wiseshare-signup-debug/static/js/runtime-main.db7487e2.js"
+  }
+]);
